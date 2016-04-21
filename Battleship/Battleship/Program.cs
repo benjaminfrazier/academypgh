4e5fda0d3e144b04dcf873539ed5aa07d08b733d
BenjaminFrazier <benjamin.james.frazier@gmail.com>
2016-04-21 12:57:16 -0400

    Test push

---
+++ Battleship/Battleship/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Create a grid(an array with two dimensions) that is 8 by 8.
+
+            //This grid will hold whether there is a ship in a given square or not.You can use a bool or an int for this.
+            //You will need to be able to display this grid, with
+            //00000000
+            //0000*000
+            //00**0000
+            //00000000
+            //000**000
+            //*0*00000
+            //00000**0
+            //00****00
+
+            //Let the user try to "shoot" a ship by selecting two coordinates (the column and the row)
+
+            //When users shoot an enemy, clear the ship in that cell
+
+            //When all enemies are gone, make the "game" end.
+
+            // Bonus:
+
+            // If user picks a cell next to a ship, say "close!"
+
+            int w, x, y, z;
+            string[,] strShip;
+            string[,] strShipLoc;
+            string[,,] shipsink;
+            String line = "";
+            string response;
+            bool keeplooping = true;
+            bool keepattacking = true;
+            string message;
+
+            strShip = new string[8, 8];
+            strShipLoc = new string[8, 8];
+            shipsink = new string[5, 8, 8];
+
+            w = 0;
+            message = "";
+
+            while (keepattacking)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to Battleship!");
+                Console.WriteLine("\n");
+
+                x = 0;
+                y = 0;
+
+                Console.Write("     X     0     1     2     3     4     5     6     7" + "\n");
+                Console.Write("\n");
+                Console.Write("Y\n");
+                Console.Write("\n");
+
+                z = 0;
+
+                while (y <= 7)
+                {
+                    while (x <= 7)
+                    {
+                        if (strShip[x,y]==null)
+                        {
+                            line = line + "      ";
+                            x = x + 1;
+                        }
+                        else
+                        {
+                            line = line + "     " + strShip[y, x];
+                            x = x + 1;
+                        }
+                        
+                    }
+
+                    Console.Write(z + "     " + line + "\n");
+                    Console.Write("\n");
+                    line = "";
+                    y = y + 1;
+                    x = 0;
+                    z = z + 1;
+                }
+
+                //C - Cruiser 2 Squares
+                strShipLoc[0, 0] = "*";
+                strShipLoc[0, 1] = "*";
+
+                //D - Destroyer Three Squares
+                strShipLoc[4, 0] = "*";
+                strShipLoc[5, 0] = "*";
+                strShipLoc[6, 0] = "*";
+
+                //S - Sub Three Squares
+                strShipLoc[7, 1] = "*";
+                strShipLoc[7, 2] = "*";
+                strShipLoc[7, 3] = "*";
+
+                //C - Battleship Four Squares
+                strShipLoc[2, 4] = "*";
+                strShipLoc[3, 4] = "*";
+                strShipLoc[4, 4] = "*";
+                strShipLoc[5, 4] = "*";
+
+                //A - Aircraft Carrier - Five Squares
+                strShipLoc[3, 6] = "*";
+                strShipLoc[4, 6] = "*";
+                strShipLoc[5, 6] = "*";
+                strShipLoc[6, 6] = "*";
+                strShipLoc[7, 6] = "*";
+
+                Console.Write("\n");
+
+                Console.WriteLine(message);
+                Console.Write("\n");
+
+                Console.WriteLine("Please enter x coordinate to attack ship!");
+                response = Console.ReadLine();
+
+                keeplooping = true;
+
+                while (keeplooping)
+                {
+                    if (int.TryParse(response, out x))
+                    {
+                        keeplooping = false;
+                    }
+                }
+
+                Console.WriteLine("Please enter y coordinate to attack ship!");
+                response = Console.ReadLine();
+
+                keeplooping = true;
+
+                while (keeplooping)
+                {
+                    if (int.TryParse(response, out y))
+                    {
+                        keeplooping = false;
+                    }
+                }
+
+                if (strShipLoc[y, x] == "*")
+                {
+                    message = "It's a hit!";
+                    strShip[y, x] = "*";
+                }
+                else
+                {
+                    message = "You missed!";
+                    strShip[y, x] = "0";
+                }
+
+                x = 0;
+                y = 0;
+                w = 0;
+
+                while (x <= 7)
+                {
+                    while (y <= 7)
+                    {
+                        if (strShipLoc[y, x] == "*" && strShip[y, x] == "*")
+                        {
+                            w = w + 1;
+                        }
+                        y = y + 1;
+                    }
+                    x = x + 1;
+                    y = 0;
+                }
+
+                if (w == 17)
+                {
+                    keepattacking = false;
+                }
+            }
+
+            Console.WriteLine("Gameover.  You sunk all the ships!!!");
+            Console.ReadLine();
+        }
+    }
+}
+
+
---
+++ RomanNumerals/RomanNumerals/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int typednum;
+            bool keeplooping;
+            string answer;
+
+            Console.WriteLine("Enter a number?");
+            typednum = Convert.ToInt16(Console.ReadLine());
+
+            answer = "";
+            keeplooping = true;
+
+            while (keeplooping)
+            {
+                //I = 1
+                //V = 5
+                //X = 10
+                //L = 50
+                //C = 100
+                //D = 500
+                //M = 1000
+
+                if (typednum>=1000)
+                {
+                    answer = answer + "M";
+                    typednum = typednum - 1000;
+                }
+                else if (typednum>= 500)
+                {
+                    if (typednum >= 900)
+                    {
+                        answer = answer + "CM";
+                        typednum = typednum - 900;
+                    }
+                    else
+                    {
+                        answer = answer + "D";
+                        typednum = typednum - 500;
+                    }
+                }
+                else if (typednum>= 100)
+                {
+                    if (typednum >= 400)
+                    {
+                        answer = answer + "CD";
+                        typednum = typednum - 400;
+                    }
+                    else
+                    {
+                        answer = answer + "C";
+                        typednum = typednum - 100;
+                    }
+                    
+                }
+                else if (typednum>= 50)
+                {
+                    if (typednum >= 90)
+                    {
+                        answer = answer + "XC";
+                        typednum = typednum - 90;
+                    }
+                    else
+                    {
+                        answer = answer + "L";
+                        typednum = typednum - 50;
+                    }
+                    
+                }
+                else if (typednum>= 10)
+                {
+                    if (typednum >= 40)
+                    {
+                        answer = answer + "XL";
+                        typednum = typednum - 40;
+                    }
+                    else
+                    {
+                        answer = answer + "X";
+                        typednum = typednum - 10;
+                    }
+                    
+                }
+                else if (typednum>= 5)
+                {
+                    if (typednum >= 9)
+                    {
+                        answer = answer + "IX";
+                        typednum = typednum - 9;
+                    }
+                    else
+                    {
+                        answer = answer + "V";
+                        typednum = typednum - 5;
+                    }
+                }
+                else if (typednum>= 1)
+                {
+                    if (typednum >= 4)
+                    {
+                        answer = answer + "IV";
+                        typednum = typednum - 4;
+                    }
+                    else
+                    {
+                        answer = answer + "I";
+                        typednum = typednum - 1;
+                    }
+                }
+                else
+                {
+                    keeplooping = false;
+                }
+            }
+
+            Console.WriteLine("Number in roman numeral format: " + answer);
+            Console.ReadLine();
+        }
+    }
+}
